78623b4cc597fdca615e0f54a92580b316cf5f01
GuiCoelhoDev <guiminervino@gmail.com>
2023-01-04 20:17:10 -0300

    Lets start

---
+++ ifood_case_utils.py
@@ -0,0 +1,20 @@
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+# import dataset from github pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import time
+
+# import dataset from github pandas
+df = pd.read_csv('https://raw.githubusercontent.com/ifood/ifood-data-business-analyst-test/master/ml_project1_data.csv', sep=',')
