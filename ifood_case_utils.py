f0377f74a037dfe39dcad6f1ac765ee470166baa
GuiCoelhoDev <guiminervino@gmail.com>
2023-01-08 23:04:49 -0300

    Start exploratory analysis

--- ifood_case_utils.py
+++ ifood_case_utils.py
@@ -18,3 +18,30 @@ import time
 
 # import dataset from github pandas
 df = pd.read_csv('https://raw.githubusercontent.com/ifood/ifood-data-business-analyst-test/master/ml_project1_data.csv', sep=',')
+
+# view NaN values of dataframe column
+df.isnull().sum()
+
+
+# Map a sqlalchemy model to dataframe column
+
+# remove rows with NaN values
+df = df.dropna()
+
+# get 5 rows with column = 1 and 5 rows with column = 0 sorted by column = 1
+
+
+df = df.groupby('column').head(5)
+
+# move column position to begin of dataframe
+# move column to begin of dataframe
+cols = df.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+df = df[cols]
+
+# get mean of a column group by another column
+df.groupby('column').mean()
+
+
+# quero saber a media de column1 quando coluna2 = 1
+df.groupby('column2').mean()
