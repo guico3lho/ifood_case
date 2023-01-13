59fc1cd691fc6084ada9f11478c1c1e11523079d
guico3lho <guiminervino@gmail.com>
2023-01-12 22:30:39 -0300

    Evaluating models

--- ifood_case_utils.py
+++ ifood_case_utils.py
@@ -1,7 +1,5 @@
 
 
-
-
 def main():
     pass
 
@@ -19,6 +17,8 @@ import time
 # import dataset from github pandas
 df = pd.read_csv('https://raw.githubusercontent.com/ifood/ifood-data-business-analyst-test/master/ml_project1_data.csv', sep=',')
 
+# for column in df.columns():
+
 
 #
 # ordinalEncoder = OrdinalEncoder(categories=categories)
@@ -35,6 +35,26 @@ df['Education_Cat'] = df['Education_Cat'].astype(int)
 
 # remove rows with missing values
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=7)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std  ())
 
 
 
